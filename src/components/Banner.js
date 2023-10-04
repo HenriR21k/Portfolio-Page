a9e9232a4afeac6ce1bddf2aec1dbc6aacec35a8
Henri <rawmroq@gmail.com>
2023-10-04 15:31:56 +0100

    In progress - Portfolio Page
    Currently have implemented the page layout, and have propped several details as content.

---
+++ src/components/Banner.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import profileImg from "../assets/img/profile.png";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/github-mark.svg';
+
+
+export const Banner = () => {
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            {/* <TrackVisibility> */}
+              {/* {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}> */}
+                {/* <span className="tagline">Welcome to my Portfolio</span> */}
+                <h1>{`Hi! I'm Henri Roquain`} </h1>
+                  <p>A passionate and analytical individual with a strong foundation in programming languages, algorithms, 
+                    data structures, and web and mobile app development technologies. 
+                    These were gained through two major projects: The Teamwork Collaboration Tool and Meal Mate which was 
+                    successfully completed during the final academic year. 
+                    I am eager to learn new technologies, take on challenging projects, 
+                    and grow professionally in a supportive and dynamic work environment. 
+                    As a Computer Science graduate, I am actively seeking a graduate position as a front-end developer.</p>
+                  <div className="profileBtn">  
+                    <button onClick={() => console.log('resume')}>View CV</button>
+                    <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                  </div>
+              {/* </div>} */}
+            {/* </TrackVisibility> */}
+          </Col>
+          <Col xs={12} md={6} xl={3}>
+            {/* <TrackVisibility>
+              {({ isVisible }) => */}
+                {/* <div className={isVisible ? "animate__animated animate__zoomIn" : ""}> */}
+                  <img src={profileImg} alt="Header Img"/>
+                    <span className="navbar-text">
+                      <div className="social-icon">
+                        <a href="#"><img src={navIcon1} alt="" /></a>
+                        <a href="#"><img src={navIcon2} alt="" /></a>
+                      </div> 
+                    </span>
+                 {/* </div>  */}
+                {/* } */}
+            {/* </TrackVisibility> */}
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,100 @@
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+
+import reactImg from "../assets/img/physics.png";
+import HTML from "../assets/img/html.png";
+import css from "../assets/img/css-3.png";
+import node from "../assets/img/nodejs.png";
+import Junit from "../assets/img/junit.png";
+import Java from "../assets/img/java.png";
+import python from "../assets/img/python.png";
+import javascript from "../assets/img/js.png";
+import express from "../assets/img/express.png";
+import mysql from "../assets/img/mysqllogo.png"
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+// import colorSharp from "../assets/img/color-sharp.png"
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <section className="skill" id="skills">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="skill-bx wow zoomIn">
+                        <h2>Skills</h2>
+                        <p>Skills developed over the past three years.</p>
+                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                            <div className="item">
+                                <img src={reactImg} alt="Image" />
+                                <h5>React/React Native</h5>
+                            </div>
+                            <div className="item">
+                                <img src={javascript} alt="Image" />
+                                <h5>JavaScript</h5>
+                            </div>
+                            <div className="item">
+                                <img src={HTML} alt="Image" />
+                                <h5>HTML</h5>
+                            </div>
+                            <div className="item">
+                                <img src={css} alt="Image" />
+                                <h5>CSS</h5>
+                            </div>
+                            <div className="item">
+                                <img src={node} alt="Image" />
+                                <h5>Node.js</h5>
+                            </div>
+                            <div className="item">
+                                <img src={express} alt="Image" />
+                                <h5>Express.js</h5>
+                            </div>
+                            <div className="item">
+                                <img src={mysql} alt="Image" />
+                                <h5>MySQL</h5>
+                            </div>
+                            <div className="item">
+                                <img src={Junit} alt="Image" />
+                                <h5>JUnit Testing</h5>
+                            </div>
+                            <div className="item">
+                                <img src={Java} alt="Image" />
+                                <h5>Java</h5>
+                            </div>
+                            <div className="item">
+                                <img src={python} alt="Image" />
+                                <h5>Python</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* <img className="background-image-left" src={colorSharp} alt="Image" /> */}
+    </section>
+  )
+}
